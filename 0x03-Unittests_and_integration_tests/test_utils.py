2f1f01b37971d48b88372d95f8a0ec55bf81b8ad
endywezy <endywezy@gmail.com>
2024-08-26 13:21:00 +0100

    Add memoize decorator to utils module
    The memoize decorator has been added to the utils module to improve code performance by caching the results of method calls. This will help optimize the execution of the a_property method in the TestMemoize class.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,7 @@
 import unittest
 from parameterized import parameterized
 from unittest.mock import patch, Mock
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -45,6 +45,26 @@ class TestGetJson(unittest.TestCase):
             self.assertEqual(response, test_payload)
 
             mock_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """Tests for the memoize decorator."""
+
+    def test_memoize(self):
+        """Test memoize to ensure method is only called once."""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+            test_instance = TestClass()
+            self.assertEqual(test_instance.a_property, 42)
+            self.assertEqual(test_instance.a_property, 42)
+            mock_method.assert_called_once()
 
 
 if __name__ == "__main__":
